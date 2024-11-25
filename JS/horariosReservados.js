68ae1f6e3e75f46f6350c35ff7a381c17e4c8741
ClasesParticularesUTN <ClasesParticularesFRSF@gmail.com>
2024-11-24 22:22:18 -0300

    Update horariosReservados.js

--- JS/horariosReservados.js
+++ JS/horariosReservados.js
@@ -73,7 +73,7 @@ var Semana1 = [];
         document.querySelector(".tablaHorarios").appendChild(loader);
 
         obtenerHorarios("0").then((celdas) => {
-            cambiarFondoCeldas(celdas);  Llamada a la función con el array
+            cambiarFondoCeldas(celdas); // Llamada a la función con el array
             Semana1 = celdas;
            // Swal.fire({
             //icon: "error",
