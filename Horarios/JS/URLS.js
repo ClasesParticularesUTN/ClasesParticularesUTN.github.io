c4c82ee71c8b5d76092035d0bb2bdd7e080183a7
Valentin <67626546+ValentinDaSilva@users.noreply.github.com>
2025-03-13 16:03:32 -0300

    Add files via upload

--- Horarios/JS/URLS.js
+++ Horarios/JS/URLS.js
@@ -1,2 +1,2 @@
 let URLHorarios = "https://script.google.com/macros/s/AKfycbz21LzDXKGY3BYZf69Tk-wGScekjTV2cQvwYtQN89FOzU8n3OZigCgrTDhjUNVNiUU/exec";
-let URLUsuarios = "https://script.google.com/macros/s/AKfycbwME4tPoZ_JcXnfL2RjAs5QCfOSbo-wKHlUUSWENBS12x9oM59u62_M7H0MW06_v7a0/exec"
+let URLUsuarios = "https://script.google.com/macros/s/AKfycbzi78k4tySBLLGz2k58Smcd68QhoaSLfAfq95xA0rgTUOlrDO-vKdlxnkDDqdlEFl4/exec";
