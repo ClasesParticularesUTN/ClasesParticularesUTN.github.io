e932723e6c819781eab8dc3b049ef9c5046902e2
Valentin <67626546+ValentinDaSilva@users.noreply.github.com>
2025-03-26 16:32:42 -0300

    Update enviarReserva.js

--- Horarios/JS/enviarReserva.js
+++ Horarios/JS/enviarReserva.js
@@ -23,17 +23,18 @@ document.querySelector(".enviarReserva").addEventListener("click", async () => {
           });
     }else if(parseInt(Alumno.dineroQueDebe.slice(1)) > 0){
         Swal.fire({
-            icon: "error",
-            title: "Error",
-            text: "Debe dinero, antes de reservar otra clase debe saldar la deuda.",
-          }).then(() => {
-            window.scrollTo(0, 0);
-            document.querySelector(".menu").style.display = "flex";
-            let listaClases = document.querySelector(".menu").classList;
-            if(!listaClases.contains("animate__zoomOutUp") && !listaClases.contains("animate__slideInDown")){
-              listaClases.add("animate__slideInDown");
-            }
+          icon: 'error',
+          title: 'Error',
+          text: 'Debe dinero, antes de reservar otra clase debe saldar la deuda.',
+        }).then(() => {
+          window.scrollTo(0, 0);
+          document.querySelector('.menu').style.display = 'flex';
+          let listaClases = document.querySelector('.menu').classList;
+          if (!listaClases.contains('animate__zoomOutUp') && !listaClases.contains('animate__slideInDown')) {
+            listaClases.add('animate__slideInDown');
+          }
         });
+
     }
     else {
         await asignarHorario(reservados,numeroHoja);
