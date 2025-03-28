fad9382b4a3ffb3fbad9cd48718441fa02cbe5dc
Valentin <67626546+ValentinDaSilva@users.noreply.github.com>
2025-03-28 00:44:44 -0300

    Update URLS.js

--- Horarios/JS/URLS.js
+++ Horarios/JS/URLS.js
@@ -1,2 +1,2 @@
 let URLHorarios = "https://script.google.com/macros/s/AKfycbz21LzDXKGY3BYZf69Tk-wGScekjTV2cQvwYtQN89FOzU8n3OZigCgrTDhjUNVNiUU/exec";
-let URLUsuarios = "https://script.google.com/macros/s/AKfycbzcmlkBQDzZYA8eI3eJunzSNXtKO9N0t2618yjpfg5N_zxiqrkrsADe_KMjij5Tz9cz/exec";
+let URLUsuarios = "https://script.google.com/macros/s/AKfycbwCQMBLyBD-VE3jLzAH2lgXFxETts7vLikIp07YSsIou_KUbru22m2IQxJ3aeaNtBfy/exec";
