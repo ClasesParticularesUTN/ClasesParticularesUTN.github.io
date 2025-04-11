c1f0b9e436fd7644d8b6510483c2b39c89560203
Valentin <valedasilvacatela.vdc@gmail.com>
2025-04-11 09:39:52 -0300

    cambio url arreglo nobre Logs

--- Horarios/JS/URLS.js
+++ Horarios/JS/URLS.js
@@ -1,2 +1,2 @@
 let URLHorarios = "https://script.google.com/macros/s/AKfycbz21LzDXKGY3BYZf69Tk-wGScekjTV2cQvwYtQN89FOzU8n3OZigCgrTDhjUNVNiUU/exec";
-let URLUsuarios = "https://script.google.com/macros/s/AKfycbyRivzQtWQGKhKucGFtuGsV0o9jhdC8pDuor47EQlRtL8sZhu4CwMLGqk1dlpuoYzdE/exec";
+let URLUsuarios = "https://script.google.com/macros/s/AKfycbxUtURboYvWmvRi3tV8XEViYMMiFTBLq9v6B2SaeoDZtCo8pMFB4fIhgrmI_ka0CRE/exec";
