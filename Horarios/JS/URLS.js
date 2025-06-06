33662d5045d1976e285e7ff78a0b5d683444f255
Valentin <valedasilvacatela.vdc@gmail.com>
2025-06-06 20:40:25 -0300

    asd

--- Horarios/JS/URLS.js
+++ Horarios/JS/URLS.js
@@ -1,2 +1,2 @@
 let URLHorarios = "https://script.google.com/macros/s/AKfycbz21LzDXKGY3BYZf69Tk-wGScekjTV2cQvwYtQN89FOzU8n3OZigCgrTDhjUNVNiUU/exec";
-let URLUsuarios = "https://script.google.com/macros/s/AKfycbx0OG1Y-fYBYTkRF0djH-TkC54_JSc7ThcWhmphWgHRVjvKpGrq2-srOChE8_DO6Eq5/exec";
+let URLUsuarios = "https://script.google.com/macros/s/AKfycbwYnKTrsulcvFoQFpxGpT3YPhwYlgh5eBBM4tq5rSpZLewN7rwxghIlaMj2otNjz8Zx/exec";
