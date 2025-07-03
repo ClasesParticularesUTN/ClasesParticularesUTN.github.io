449051a8334e0a1657d84e2dd2cef142cfdfc5ee
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2025-07-03 17:48:07 -0300

    asdf

--- Horarios/JS/URLS.js
+++ Horarios/JS/URLS.js
@@ -1,2 +1,2 @@
 let URLHorarios = "https://script.google.com/macros/s/AKfycbzXrIHvxAxfjj-xGjd67UH0hAmO2QoO4j503pT6tuEF7kIq2OLt-iPhb8Nk4r6F0g/exec";
-let URLUsuarios = "https://script.google.com/macros/s/AKfycbxc2mPTWXpHnzO3-oiwZ1pToPwE04OSR1YxwyhTYoQsJf7lZc9F3ZgMSVK_wEbtkcWL/exec";
+let URLUsuarios = "https://script.google.com/macros/s/AKfycbzuwUsGy2R6Yfu_I9Uc1CH_76uA6g0Cp2fagoVojrauw_Zqy2qoIz9QHZ7DBJqm60FF/exec";
