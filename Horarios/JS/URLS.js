2c6430031e656c63eb15ddb61f556d533af53d91
Valentin <valedasilvacatela.vdc@gmail.com>
2025-08-13 11:37:48 -0300

    asd

--- Horarios/JS/URLS.js
+++ Horarios/JS/URLS.js
@@ -1,3 +1,2 @@
-let URLHorarios = "https://script.google.com/macros/s/git config --global user.name "Tu Nombre"
-AKfycbzayxbAn7m7tZHk4tVVcrR-QqRL9nrbIiI6FEAjCMiPHzveV6aA_R9L1i4AUTZD0d8/exec";
+let URLHorarios = "https://script.google.com/macros/s/AKfycbzayxbAn7m7tZHk4tVVcrR-QqRL9nrbIiI6FEAjCMiPHzveV6aA_R9L1i4AUTZD0d8/exec";
 let URLUsuarios = "https://script.google.com/macros/s/AKfycbyAQ6gejD1_enMPt0vBm78j6itQRtK-XejK10XkLgP-l0OkD6e6Zy0JJhiA9shYiE2F/exec";
