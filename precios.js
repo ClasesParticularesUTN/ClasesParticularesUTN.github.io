eb5405500c0a52918c8d50c950a2114410524fdb
Valentin <67626546+ValentinDaSilva@users.noreply.github.com>
2026-05-04 13:32:25 -0300

    Update precios.js

--- precios.js
+++ precios.js
@@ -19,13 +19,13 @@ function normalizarTexto(texto) {
  */
 const PRECIOS_BASE = {
     individual: {
-        expres1h: 5000,
-        estandar2h: 9000,
-        precioPorHora1: 5000,
-        precioPorHora2: 4500,
-        packExamen: 9000 * 8,
-        packMateria: 9000 * 14
-    },
+            expres1h: 5500,
+            estandar2h: 10000,
+            precioPorHora1: 5500,
+            precioPorHora2: 5000,
+            packExamen: 80000,
+            packMateria: 160000,
+        },
     grupal: {
         estandar2h: 6000,
         precioPorHora: 3000,
