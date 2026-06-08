534dbf06ffa7ee8803a8a1ebc239931ca58204db
Valentin <valedasilvacatela.vdc@gmail.com>
2026-06-08 12:43:53 -0300

    asd

--- precios.js
+++ precios.js
@@ -25,7 +25,7 @@ const PRECIOS_BASE = {
             precioPorHora2: 5000,
             packExamen: 10000 * 8,
             packMateria: 10000 *14,
-        },
+    },
     grupal: {
         estandar2h: 7000,
         precioPorHora: 3500,
@@ -75,7 +75,17 @@ const PRECIOS_ESPECIALES = {
             packExamen: 10,
             packMateria: 20,
         }
+    },
+    "Benitez Juan Martin": {
+        individual: {
+            expres1h: 6500,
+            estandar2h: 10000,
+            precioPorHora1: 6500,
+            precioPorHora2: 5000,
+            packExamen: 9000 * 8,
+            packMateria: 9000 *14,
     }
+    }    
 };
 
 
