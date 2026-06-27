ca626cd0c58f595faff06274df191206c45d4757
Valentin <valedasilvacatela.vdc@gmail.com>
2026-06-27 16:36:51 -0300

    asd

--- Horarios/preciosActualizados.js
+++ Horarios/preciosActualizados.js
@@ -1,6 +1,6 @@
 // Correos de alumnos que verán el badge "NUEVO!" en los precios actualizados
 const CORREOS_PRECIOS_ACTUALIZADOS = [
     "valedasilvacatela.vdc@gmail.com",
-    "paulinalisvillanueva@gmail.com",
-    "andresnigorrafac2026@gmail.com"
+    "paulinalisvillanuev@gmail.com",
+    "andresnigorrafac202@gmail.com"
 ];
