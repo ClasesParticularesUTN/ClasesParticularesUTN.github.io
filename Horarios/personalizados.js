0039b2027d4315032d831984a13116dc77056b60
Valentin <valedasilvacatela.vdc@gmail.com>
2026-07-08 22:12:47 -0300

    asd

--- Horarios/personalizados.js
+++ Horarios/personalizados.js
@@ -62,6 +62,36 @@ const HORARIOS_PERSONALIZADOS = {
     },
     "mirko.scharfspitz@gmail.com": {
         habilitadas: [],
+        
+        habilitadas: [
+
+            // Habilita todo el rango B5:D12 para semana 1
+            { semana: 1, celda: "B5" },
+            { semana: 1, celda: "B6" },
+            { semana: 1, celda: "B7" },
+            { semana: 1, celda: "B8" },
+            { semana: 1, celda: "B9" },
+            { semana: 1, celda: "B10" },
+            { semana: 1, celda: "B11" },
+            { semana: 1, celda: "B12" },
+            { semana: 1, celda: "C5" },
+            { semana: 1, celda: "C6" },
+            { semana: 1, celda: "C7" },
+            { semana: 1, celda: "C8" },
+            { semana: 1, celda: "C9" },
+            { semana: 1, celda: "C10" },
+            { semana: 1, celda: "C11" },
+            { semana: 1, celda: "C12" },
+            { semana: 1, celda: "D5" },
+            { semana: 1, celda: "D6" },
+            { semana: 1, celda: "D7" },
+            { semana: 1, celda: "D8" },
+            { semana: 1, celda: "D9" },
+            { semana: 1, celda: "D10" },
+            { semana: 1, celda: "D11" },
+            { semana: 1, celda: "D12" },
+    
+        ],
         deshabilitadas: [
         { semana: 1, celda: "E3" },
         { semana: 1, celda: "E4" },
@@ -89,7 +119,36 @@ const HORARIOS_PERSONALIZADOS = {
         ]
     },
     "matisoldini1@gmail.com": {
-        habilitadas: [],
+        
+        habilitadas: [
+
+            // Habilita todo el rango B5:D12 para semana 1
+            { semana: 1, celda: "B5" },
+            { semana: 1, celda: "B6" },
+            { semana: 1, celda: "B7" },
+            { semana: 1, celda: "B8" },
+            { semana: 1, celda: "B9" },
+            { semana: 1, celda: "B10" },
+            { semana: 1, celda: "B11" },
+            { semana: 1, celda: "B12" },
+            { semana: 1, celda: "C5" },
+            { semana: 1, celda: "C6" },
+            { semana: 1, celda: "C7" },
+            { semana: 1, celda: "C8" },
+            { semana: 1, celda: "C9" },
+            { semana: 1, celda: "C10" },
+            { semana: 1, celda: "C11" },
+            { semana: 1, celda: "C12" },
+            { semana: 1, celda: "D5" },
+            { semana: 1, celda: "D6" },
+            { semana: 1, celda: "D7" },
+            { semana: 1, celda: "D8" },
+            { semana: 1, celda: "D9" },
+            { semana: 1, celda: "D10" },
+            { semana: 1, celda: "D11" },
+            { semana: 1, celda: "D12" },
+    
+        ],
         deshabilitadas: [
         { semana: 1, celda: "E3" },
         { semana: 1, celda: "E4" },
@@ -114,19 +173,6 @@ const HORARIOS_PERSONALIZADOS = {
         { semana: 1, celda: "F12" },
         { semana: 1, celda: "F13" },
    
-        ]
-    },
-    "juanma.2000@hotmail.com": {
-        habilitadas: [
-            { semana: 0, celda: "F11" },
-            { semana: 0, celda: "F12" },
-            { semana: 0, celda: "F13" },
-            { semana: 0, celda: "F5" },
-            { semana: 0, celda: "F6" },
-   
-        ],
-        deshabilitadas: [
-            
         ]
     },
     "bautistaclaret10@gmail.com": {
