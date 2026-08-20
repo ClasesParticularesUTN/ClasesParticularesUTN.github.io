1d56e14e2c9fd438154a901fbf7e85a7a0374887
Valentin <valedasilvacatela.vdc@gmail.com>
2026-08-20 19:25:33 -0300

    asd

--- Algoritmos/Videos/permitidos.js
+++ Algoritmos/Videos/permitidos.js
@@ -1,5 +1,30 @@
-const CORREOS_PERMITIDOS = [
-    "valedasilvacatela.vdc@gmail.com",
-    "clasesparticularesutnfrsf@gmail.com",
-    "paulinalisvillanueva@gmail.com"
-];
+const PERMISOS_POR_CORREO = {
+    "valedasilvacatela.vdc@gmail.com": {
+        burbuja: true,
+        insercion: true,
+        mergeSort: true,
+        seleccion: true,
+        clockFrecuencia: true
+    },
+    "clasesparticularesutnfrsf@gmail.com": {
+        burbuja: true,
+        insercion: true,
+        mergeSort: true,
+        seleccion: true,
+        clockFrecuencia: true
+    },
+    "paulinalisvillanueva@gmail.com": {
+        burbuja: false,
+        insercion: false,
+        mergeSort: false,
+        seleccion: false,
+        clockFrecuencia: true
+    },
+    "vmosquen@gmail.com": {
+        burbuja: false,
+        insercion: true,
+        mergeSort: false,
+        seleccion: false,
+        clockFrecuencia: true
+    }
+};
