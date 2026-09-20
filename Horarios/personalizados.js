93ff0df44f07e1fd8c897083c410423af24fd2de
Valentin <valedasilvacatela.vdc@gmail.com>
2026-09-20 19:07:33 -0300

    asd

--- Horarios/personalizados.js
+++ Horarios/personalizados.js
@@ -73,28 +73,14 @@ const HORARIOS_PERSONALIZADOS = {
     "sebasmaina@gmail.com": {
         habilitadas: [],
         deshabilitadas: [
-            { semana: 0, celda: "B5:B13" },
-            { semana: 1, celda: "B5:B13" },
-            
-            { semana: 0, celda: "F6:F13" },
-            { semana: 1, celda: "F6:F13" },
-            
-            { semana: 0, celda: "D3:D13" },
-            { semana: 1, celda: "D3:D13" },
+            { semana: 0, celda: "B3:F13" },
 
         ]
     },
     "renzomercanti2005@gmail.com": {
         habilitadas: [],
         deshabilitadas: [
-            { semana: 0, celda: "B5:B13" },
-            { semana: 1, celda: "B5:B13" },
-            
-            { semana: 0, celda: "F6:F13" },
-            { semana: 1, celda: "F6:F13" },
-            
-            { semana: 0, celda: "D3:D13" },
-            { semana: 1, celda: "D3:D13" },
+            { semana: 0, celda: "B3:F13" },
 
         ]
     },
